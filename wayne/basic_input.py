e32aa16678de85c1e6358da3605efcf7d0752fd6
Wayne Folkes <wayne@Waynes-MBP-2012.home>
2014-06-27 00:03:46 -0400

    another example

--- wayne/basic_input.py
+++ wayne/basic_input.py
@@ -3,7 +3,7 @@
 import sys
 
 #using string formatting
-print 'hello %s' % sys.argv[1]
+print 'hello you called a program called %s and you passed in %s as an argument' % (sys.argv[0], sys.argv[1])
 
 #not using string formatting
-print 'hello ' + sys.argv[1]
+print 'hello you called a program called ' + sys.argv[0] + ' and you passed in ' + sys.argv[1] + ' as an argument'
