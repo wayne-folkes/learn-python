b64e050572c1538a91e0b4bc45898af7320b7324
Wayne Folkes <wayne@Waynes-MBP-2012.home>
2014-06-27 00:32:52 -0400

    example with a hash

--- wayne/basic_input.py
+++ wayne/basic_input.py
@@ -3,7 +3,7 @@
 import sys
 
 #This is a list
-simple_array = {'wayne' : 'knows some python', 'david' : 'is learning some python'}
+simple_hash = {'wayne' : 'knows some python', 'david' : 'is learning some python'}
 
 #using string formatting
 print 'hello you called a program called %s and you passed in %s as an argument' % (sys.argv[0], sys.argv[1])
@@ -12,7 +12,7 @@ print 'hello you called a program called %s and you passed in %s as an argument'
 print 'hello you called a program called ' + sys.argv[0] + ' and you passed in ' + sys.argv[1] + ' as an argument'
 
 #this is annoying 
-print "'Wayne " + simple_array['wayne'] + " and David " + simple_array['david'] + "'"
+print "'Wayne " + simple_hash['wayne'] + " and David " + simple_hash['david'] + "'"
 
 #this is simpler
-print "'Wayne %s and David %s'" % ( simple_array['wayne'], simple_array['david'])
+print "'Wayne %s and David %s'" % ( simple_hash['wayne'], simple_hash['david'])
