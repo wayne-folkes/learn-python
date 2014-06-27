09351a6aca0545765c964b67f97813608ef3f062
Wayne Folkes <wayne@Waynes-MBP-2012.home>
2014-06-27 00:36:10 -0400

    commenting out

--- wayne/basic_input.py
+++ wayne/basic_input.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
-import sys
+#import sys
 
 #This is a list
 simple_hash = {'wayne' : 'knows some python', 'david' : 'is learning some python'}
 
 #using string formatting
-print 'hello you called a program called %s and you passed in %s as an argument' % (sys.argv[0], sys.argv[1])
+#print 'hello you called a program called %s and you passed in %s as an argument' % (sys.argv[0], sys.argv[1])
 
 #not using string formatting
-print 'hello you called a program called ' + sys.argv[0] + ' and you passed in ' + sys.argv[1] + ' as an argument'
+#print 'hello you called a program called ' + sys.argv[0] + ' and you passed in ' + sys.argv[1] + ' as an argument'
 
 #this is annoying 
 print "'Wayne " + simple_hash['wayne'] + " and David " + simple_hash['david'] + "'"
